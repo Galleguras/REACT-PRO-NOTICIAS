42e7618221dd77c1aabf9f0dc67d1de7a7c97600
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-05-14 11:25:30 +0200

    OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
 function App() {
+
+  // definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=747226819bdd4db7a020cbb27f3bfe02&apiKey=747226819bdd4db7a020cbb27f3bfe02`;
+     
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      console.log(noticias.articles);
+     
+      guardarNoticias(noticias.articles);
+    };
+    consultarApi();
+  }, [categoria]);
+
   return (
-   <h1>Noticias</h1>
+    <Fragment>
+        <Header 
+          titulo='Buscador de Noticias'
+        />
+
+        <div className="container white">
+            <Formulario 
+              guardarCategoria={guardarCategoria}
+            />
+
+            <ListadoNoticias 
+              noticias={noticias}
+            />
+        </div>
+    </Fragment>
   );
 }
 
